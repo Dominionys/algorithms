d18db6ade4b3629c79ce70f1a0d4ac1b8871da34
Dominionys <denis_bezrukov@mail.ru>
2021-03-04 08:30:24 +0200

    add queue

--- queues/src/queue.rs
+++ queues/src/queue.rs
@@ -1,10 +1,10 @@
-pub struct Stack<T> {
+pub struct Queue<T> {
     items: Vec<T>,
 }
 
-impl<T> Stack<T> {
-    pub fn new() -> Stack<T> {
-        Stack { items: Vec::new() }
+impl<T> Queue<T> {
+    pub fn new() -> Queue<T> {
+        Queue { items: Vec::new() }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -15,103 +15,106 @@ impl<T> Stack<T> {
         self.items.len()
     }
 
-    pub fn push(&mut self, element: T) {
+    pub fn enqueue(&mut self, element: T) {
         self.items.push(element);
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        self.items.pop()
+    pub fn dequeue(&mut self) -> Option<T> {
+        match self.is_empty() {
+            false => Some(self.items.remove(0)),
+            true => None,
+        }
     }
 
     pub fn peek(&self) -> Option<&T> {
-        self.items.last()
+        self.items.first()
     }
 }
 
-impl<T> Iterator for Stack<T> {
+impl<T> Iterator for Queue<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.pop()
+        self.dequeue()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Stack;
+    use super::Queue;
 
     #[test]
     fn test_empty() {
-        let mut stack: Stack<i32> = Stack::new();
+        let mut stack: Queue<i32> = Queue::new();
 
         assert_eq!(stack.is_empty(), true);
         assert_eq!(stack.size(), 0);
         assert_eq!(stack.peek(), None);
-        assert_eq!(stack.pop(), None);
+        assert_eq!(stack.dequeue(), None);
     }
 
     #[test]
     fn test_one_element() {
-        let mut stack: Stack<i32> = Stack::new();
+        let mut stack: Queue<i32> = Queue::new();
 
-        stack.push(123);
+        stack.enqueue(123);
 
         assert_eq!(stack.is_empty(), false);
         assert_eq!(stack.size(), 1);
         assert_eq!(stack.peek(), Some(&123));
 
-        let result = stack.pop();
+        let result = stack.dequeue();
 
         assert_eq!(result, Some(123));
         assert_eq!(stack.is_empty(), true);
         assert_eq!(stack.size(), 0);
         assert_eq!(stack.peek(), None);
-        assert_eq!(stack.pop(), None);
+        assert_eq!(stack.dequeue(), None);
     }
 
     #[test]
     fn test_two_element() {
-        let mut stack: Stack<i32> = Stack::new();
+        let mut stack: Queue<i32> = Queue::new();
 
-        stack.push(123);
-        stack.push(456);
+        stack.enqueue(123);
+        stack.enqueue(456);
 
         assert_eq!(stack.is_empty(), false);
         assert_eq!(stack.size(), 2);
-        assert_eq!(stack.peek(), Some(&456));
-
-        let result = stack.pop();
-
-        assert_eq!(result, Some(456));
-        assert_eq!(stack.is_empty(), false);
-        assert_eq!(stack.size(), 1);
         assert_eq!(stack.peek(), Some(&123));
 
-        let result = stack.pop();
+        let result = stack.dequeue();
 
         assert_eq!(result, Some(123));
+        assert_eq!(stack.is_empty(), false);
+        assert_eq!(stack.size(), 1);
+        assert_eq!(stack.peek(), Some(&456));
+
+        let result = stack.dequeue();
+
+        assert_eq!(result, Some(456));
         assert_eq!(stack.is_empty(), true);
         assert_eq!(stack.size(), 0);
         assert_eq!(stack.peek(), None);
-        assert_eq!(stack.pop(), None);
+        assert_eq!(stack.dequeue(), None);
     }
 
     #[test]
     fn test_iterator() {
-        let mut stack: Stack<i32> = Stack::new();
+        let mut stack: Queue<i32> = Queue::new();
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
-        stack.push(6);
+        stack.enqueue(1);
+        stack.enqueue(2);
+        stack.enqueue(3);
+        stack.enqueue(4);
+        stack.enqueue(5);
+        stack.enqueue(6);
 
         let mut result: Vec<i32> = Vec::new();
         for element in stack {
             result.push(element);
         }
 
-        assert_eq!(result, [6, 5, 4, 3, 2, 1]);
+        assert_eq!(result, [1, 2, 3, 4, 5, 6]);
     }
 }
